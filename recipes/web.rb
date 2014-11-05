71a9df98e04bbb2a9e18d887ba68457afc36dde1
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-05 15:32:35 +0530

    updated time for web workers

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,6 +1,6 @@
 cron 'CRON - image versions' do
-	hour '6'
-	minute '35'
+	hour '11'
+	minute '0'
 	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
 	action :create
