a17c0e5b52631d2707e4c48b73e14e7f8a5d2b1b
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-05 13:30:46 +0530

    adding cron jobs as root

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,7 +1,7 @@
 cron 'CRON - image versions' do
 	hour '6'
 	minute '35'
-	user 'ubuntu'
+	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
 	action :create
 end
