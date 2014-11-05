701cc47935724762add32c25ff81490f8acab54b
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-05 11:54:29 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'horntell-crons'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures horntell-crons'
+long_description 'Installs/Configures horntell-crons'
+version          '0.1.0'
+
