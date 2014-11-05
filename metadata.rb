e8d2aaf0b78f6631ce78401dbdfb0e7617875042
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-05 12:45:31 +0530

    bumped up the version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures horntell-crons'
 long_description 'Installs/Configures horntell-crons'
-version          '0.1.0'
+version          '0.1.1'
 
