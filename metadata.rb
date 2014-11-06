734c4dc2a97ade2a0687118c9ed8d59099b6925d
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-06 17:07:52 +0530

    added author

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'horntell-crons'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Mohit Mamoria'
+maintainer_email 'mohit.mamoria@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures horntell-crons'
 long_description 'Installs/Configures horntell-crons'
