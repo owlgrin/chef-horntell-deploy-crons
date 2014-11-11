7306edb1da9178d0841097874b5ac306784818f6
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-11-11 15:05:50 +0530

    running cronjob every half hour

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,6 +1,5 @@
 cron 'CRON - image versions' do
-	hour '11'
-	minute '0'
+	minute '*/30'
 	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
 	action :create
