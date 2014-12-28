ce259745dbdf82e0a29a1128c39a075bb62eb0dd
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-12-28 15:03:45 +0530

    Removed the user ubuntu specification

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,6 @@
 # */30 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan campaign:fire --env=production > /var/log/horntell/campaign.fire.log
 cron 'CRON - campaign fire' do
 	minute '*/30'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan campaign:fire --env=production > /var/log/horntell/campaign.fire.log"
 	action :create
 end
@@ -10,7 +9,6 @@ end
 cron 'CRON - metrics seed' do
 	minute '30'
 	hour '0'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan metrics:seed --env=production > /var/log/horntell/metrics.seed.log"
 	action :create
 end
@@ -19,7 +17,6 @@ end
 cron 'CRON - segments refresh' do
 	minute '0'
 	hour '0'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan segments:refresh --env=production > /var/log/horntell/segments.refresh.log"
 	action :create
 end
@@ -28,7 +25,6 @@ end
 cron 'CRON - segments count' do
 	minute '10'
 	hour '0'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan segments:count --env=production > /var/log/horntell/segments.count.log"
 	action :create
 end
@@ -37,7 +33,6 @@ end
 # */2 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan campaign:last-fires --env=production > /var/log/horntell/campaign.last-fires.log
 cron 'CRON - campaign last fires' do
 	minute '*/2'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan campaign:last-fires --env=production > /var/log/horntell/campaign.last-fires.log"
 	action :create
 end
@@ -55,7 +50,6 @@ end
 # */5 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan horn:stats --env=production > /var/log/horntell/horn.stats.log
 cron 'CRON - horn stats' do
 	minute '*/5'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan horn:stats --env=production > /var/log/horntell/horn.stats.log"
 	action :create
 end
@@ -63,7 +57,6 @@ end
 # */30 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan campaign:autorun --env=production > /var/log/horntell/campaign.autorun.log
 cron 'CRON - campaign autorun' do
 	minute '*/30'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan campaign:autorun --env=production > /var/log/horntell/campaign.autorun.log"
 	action :create
 end
@@ -71,7 +64,6 @@ end
 # */30 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan campaign:date-trigger --env=production > /var/log/horntell/campaign.date-trigger.log
 cron 'CRON - campaign date-trigger' do
 	minute '*/30'
-	# user 'ubuntu'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan campaign:date-trigger --env=production > /var/log/horntell/campaign.date-trigger.log"
 	action :create
 end
