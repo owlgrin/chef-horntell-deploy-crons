922b91732983fd6647420c8e9213ab05670b1e73
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-06 03:12:25 +0530

    added comments

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,3 +1,5 @@
+# we will create the multiple versions of the images on the machine on which,
+# it was uploaded itself.
 cron 'CRON - image versions' do
 	minute '*/30'
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
