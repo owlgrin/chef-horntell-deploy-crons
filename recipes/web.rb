c174379ecaffe30653ee37d7f60a397bb8b0da4b
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-07 23:15:15 +0530

    added details about the cron command

--- recipes/web.rb
+++ recipes/web.rb
@@ -2,6 +2,7 @@
 # it was uploaded itself.
 cron 'CRON - image versions' do
 	minute '*/30'
+	# right now, only AppLogo is being pushed to the S3. More should be added here.
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
 	action :create
 end
