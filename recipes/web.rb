53d62a923a74b142006c29234a2155e8dc5a4820
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-17 00:42:53 +0530

    described cron times of web recipe too

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,7 +1,7 @@
 # we will create the multiple versions of the images on the machine on which,
 # it was uploaded itself.
 cron 'CRON - image versions' do
-	minute '*/30'
+	minute '*/30' # every 30 minutes
 	# right now, only AppLogo is being pushed to the S3. More should be added here.
 	command "/usr/bin/php /home/ubuntu/apps/core/artisan image:versions AppLogo --clean --env=production > /var/log/horntell/image.versions.log"
 	action :create
