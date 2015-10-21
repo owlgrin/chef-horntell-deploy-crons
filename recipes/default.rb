df55bde8c25b547c165dd3c98cb811acbc762518
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-10-21 11:58:55 +0530

    removed the throttle, cashew cron jobs

--- recipes/default.rb
+++ recipes/default.rb
@@ -55,12 +55,12 @@ cron 'CRON - horn stats' do
 end
 
 # 0 0 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan horntell:remind-referrer --env=production > /var/log/horntell/referral.remind-referrer.log
-cron 'CRON - horn remind referrer' do
-	minute '0' # at 00:00
-	minute '0' # at 00:00
-	command "/usr/bin/php /home/ubuntu/apps/core/artisan horntell:remind-referrer --env=production > /var/log/horntell/referral.remind-referrer.log"
-	action :create
-end
+# cron 'CRON - horn remind referrer' do
+# 	minute '0' # at 00:00
+# 	minute '0' # at 00:00
+# 	command "/usr/bin/php /home/ubuntu/apps/core/artisan horntell:remind-referrer --env=production > /var/log/horntell/referral.remind-referrer.log"
+# 	action :create
+# end
 
 # */30 * * * * /usr/bin/php /home/rhino/code/horntell/core/artisan campaign:autorun --env=production > /var/log/horntell/campaign.autorun.log
 # cron 'CRON - campaign autorun' do
@@ -77,36 +77,36 @@ end
 # end
 
 # 0 23 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan throttle:seed-base-usage --env=production > /var/log/horntell/throttle.seed-base-usage.log
-cron 'CRON - throttle seed base usage' do
-	minute '0' # at 2300 everyday
-	hour '23' # at 2300 everyday
-	command "/usr/bin/php /home/ubuntu/apps/core/artisan throttle:seed-base-usage --env=production > /var/log/horntell/throttle.seed-base-usage.log"
-	action :create
-end
+# cron 'CRON - throttle seed base usage' do
+# 	minute '0' # at 2300 everyday
+# 	hour '23' # at 2300 everyday
+# 	command "/usr/bin/php /home/ubuntu/apps/core/artisan throttle:seed-base-usage --env=production > /var/log/horntell/throttle.seed-base-usage.log"
+# 	action :create
+# end
 
 # 0 12 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan throttle:add-subscription-period --env=production > /var/log/horntell/throttle.add-subscription-period.log
-cron 'CRON - throttle add next subscription period' do
-	minute '0' # at 1200 everyday
-	hour '12' # at 1200 everyday
-	command "/usr/bin/php /home/ubuntu/apps/core/artisan throttle:add-subscription-period --env=production > /var/log/horntell/throttle.add-subscription-period.log"
-	action :create
-end
+# cron 'CRON - throttle add next subscription period' do
+# 	minute '0' # at 1200 everyday
+# 	hour '12' # at 1200 everyday
+# 	command "/usr/bin/php /home/ubuntu/apps/core/artisan throttle:add-subscription-period --env=production > /var/log/horntell/throttle.add-subscription-period.log"
+# 	action :create
+# end
 
 # 0 1 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan cashew:expire --env=demo > /var/log/horntell/cashew.expire.log
-cron 'CRON - expire the canceled subscriptions' do
-	minute '0' # at 0100 everyday
-	hour '1' # at 0100 everyday
-	command "/usr/bin/php /home/ubuntu/apps/core/artisan cashew:expire --env=production > /var/log/horntell/cashew.expire.log"
-	action :create
-end
+# cron 'CRON - expire the canceled subscriptions' do
+# 	minute '0' # at 0100 everyday
+# 	hour '1' # at 0100 everyday
+# 	command "/usr/bin/php /home/ubuntu/apps/core/artisan cashew:expire --env=production > /var/log/horntell/cashew.expire.log"
+# 	action :create
+# end
 
 # 0 12 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan cashew:ping-user-expiring-card 30 15 7 3 1 --env=production > /var/log/horntell/cashew.ping-expiring-cards.log
-cron 'CRON - ping users with expiring cards' do
-	minute '0' # at 1200 everyday
-	hour '12' # at 1200 everyday
-	command "/usr/bin/php /home/ubuntu/apps/core/artisan cashew:ping-user-expiring-card 30 29 15 7 3 1 --env=production > /var/log/horntell/cashew.ping-expiring-cards.log" # intervals are separated with spaces
-	action :create
-end
+# cron 'CRON - ping users with expiring cards' do
+# 	minute '0' # at 1200 everyday
+# 	hour '12' # at 1200 everyday
+# 	command "/usr/bin/php /home/ubuntu/apps/core/artisan cashew:ping-user-expiring-card 30 29 15 7 3 1 --env=production > /var/log/horntell/cashew.ping-expiring-cards.log" # intervals are separated with spaces
+# 	action :create
+# end
 
 # 0 12 * * * /usr/bin/php /home/rhino/code/horntell/core/artisan dripper:send --env=production > /var/log/horntell/dripper.send.log
 cron 'CRON - send drip emails' do
